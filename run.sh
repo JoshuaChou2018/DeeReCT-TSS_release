2d172414b6aed918c6b73331b8d2537334da3fd0
christear <zb.picb@gmail.com>
2021-08-17 11:53:51 +0300

    v1_for_immplemention
    v1

---
+++ run.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+:<<USE
+USE
+
+function usage () {
+        echo
+        echo
+		echo Usage:run.sh [bam_file] [bed_regions] [model] [genome_fa] [output_directory] [0/1/2 ifstranded] [thread]
+        echo
+		echo
+}
+
+if [ -z $6 ]; then
+        usage
+        exit
+fi
+
+bam=${1}
+bed_reg=${2}
+model=${3}
+genome=${4}
+out_dir=${5}
+IFSTR=${6}
+thread=${7}
+
+##
+#conda activate tss
+module load bedtools
+##
+
+echo processing bam file $bam ...
+#:<<FIN
+#date
+if [ $IFSTR == 1 ]; then
+        echo the sequencing data is from sense strand ... 
+        genomeCoverageBed -ibam $bam -split -dz -strand + | awk '{OFS="\t"}{print $1,$2,$2+1,$1":"$2,$3,"+"}' | grep "chr" > $out_dir/RNAseq.depth.bed
+        genomeCoverageBed -ibam $bam -split -dz -strand - | awk '{OFS="\t"}{print $1,$2,$2+1,$1":"$2,$3,"-"}' | grep "chr" >> $out_dir/RNAseq.depth.bed
+elif [ $IFSTR == 2 ]; then
+        echo the sequencing data is from antisense strand ...
+        genomeCoverageBed -ibam $bam -split -dz -strand - | awk '{OFS="\t"}{print $1,$2,$2+1,$1":"$2,$3,"+"}' | grep "chr" > $out_dir/RNAseq.depth.bed
+        genomeCoverageBed -ibam $bam -split -dz -strand + | awk '{OFS="\t"}{print $1,$2,$2+1,$1":"$2,$3,"-"}' | grep "chr" >> $out_dir/RNAseq.depth.bed
+else
+        echo the sequencing data is strandless ...
+        genomeCoverageBed -ibam $bam -split -dz -strand + | awk '{OFS="\t"}{print $1,$2,$2+1,$1":"$2,$3,"+"}' | grep "chr" > $out_dir/RNAseq.depth.bed
+		genomeCoverageBed -ibam $bam -split -dz -strand - | awk '{OFS="\t"}{print $1,$2,$2+1,$1":"$2,$3,"-"}' | grep "chr" >> $out_dir/RNAseq.depth.bed
+fi
+#date
+#FIN
+
+echo splitting files 4 parallelization ...
+split -n l/$thread -d $out_dir/RNAseq.depth.bed $out_dir/RNAseq.depth.part.
+rm -f $out_dir/RNAseq.depth.bed
+#date
+for eachp in $out_dir/RNAseq.depth.part*
+do
+	echo processing $eachp ...
+#	date
+	bash run1thread.sh $eachp $bed_reg $model $genome $out_dir &
+	sleep 2s
+#	date
+done
+wait
+#date
+echo merging the output cluster ...
+cat $out_dir/RNAseq.depth.part*cluster | ntersectBed -a - -b $bed_reg -s -wo | awk '{OFS="\t"}{print $1,$2,$3,$4,$11,$6,$7}' > $out_dir/combined.predicted.cluster
+
+
+
+
+
+
+
+
---
+++ run1thread.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+:<<USE
+USE
+
+function usage () {
+        echo
+        echo
+		echo Usage:run1thread.sh [depth_bed] [bed_regions] [model] [genome_fa] [output_directory]
+        echo
+		echo
+}
+
+if [ -z $5 ]; then
+        usage
+        exit
+fi
+
+depth_bed=${1}
+bed_reg=${2}
+model=${3}
+genome=${4}
+out_dir=${5}
+
+grep "+" $depth_bed | intersectBed -a - -b $bed_reg -s -u | cut -f 1,2,5 > $depth_bed.plus
+grep "-" $depth_bed | intersectBed -a - -b $bed_reg -s -u | cut -f 1,2,5 > $depth_bed.minus
+
+echo extracting genomic regions 4 scanning ...
+# only keep the loci with depth > 1 read 
+awk '$5 > 1' $depth_bed | intersectBed -a - -b $bed_reg -s -wo | awk '{OFS="\t"}{print $1,$2 - 1000,$3 + 1000,$10,$5,$12}' | awk '$2 > 0' | sort | uniq | sortBed -i - > $depth_bed.sorted
+mergeBed -i $depth_bed.sorted -s -c 4,5,6 -o distinct,sum,distinct | awk '$2 > 500' > $depth_bed.merged
+### 
+echo scanning the genome ...
+python src/05.scan.app.v2.py $depth_bed.merged $out_dir $model $depth_bed.plus $depth_bed.minus $genome 5 0 0.1 1>$depth_bed.scan.log 2>$depth_bed.scan.elog
+
+scut=0.1
+dcut=10
+prop=0.971
+
+echo clustering the predictions ...
+echo change to 0-based bed format 
+awk -v scut=$scut '{OFS="\t"}{if($4 > scut) print $1,$2 - 1,$3,"c",$4,$5}' $depth_bed.merged.bedgraph \
+	 | grep -v "chrM" | sortBed -i | mergeBed -i - -s -d $dcut -c 4,5,6 -o first,sum,distinct > $depth_bed.rawout
+awk -v prop=$prop '{OFS="\t"}{print $1,int(($2+$3)/2),int(($2+$3)/2) + 1,$5,$4,$6,prop^int($5)}' $depth_bed.rawout | awk '$5 > 1' | sort -k 4 -r -n  > $depth_bed.cluster
+rm -f $depth_bed
+rm -f $bed_reg
+rm -f $depth_bed.sorted
+rm -f $depth_bed.
+rm -f $depth_bed.rawout
+#rm -f $depth.mergeBed.bedgraph
+
+
+
+
